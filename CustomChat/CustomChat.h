b0d1ce8e07ae25ddfe8c6226151d3aee5cfda817
Ari Abrams <ariabrams65@gmail.com>
2023-04-08 13:55:27 -0700

    Messages can be sent

--- CustomChat/CustomChat.h
+++ CustomChat/CustomChat.h
@@ -10,36 +10,22 @@ constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_M
 
 class CustomChat: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginSettingsWindow*//*, public BakkesMod::Plugin::PluginWindow*/
 {
-
-	//std::shared_ptr<bool> enabled;
-
-	//Boilerplate
+public:
 	virtual void onLoad();
 	virtual void onUnload();
+
+private:
+	void sendMessage(const std::string&);
+	void pressKey(char);
+	void pressVk(short);
+
+
 
 	// Inherited via PluginSettingsWindow
 	/*
 	void RenderSettings() override;
 	std::string GetPluginName() override;
 	void SetImGuiContext(uintptr_t ctx) override;
-	*/
-
-	// Inherited via PluginWindow
-	/*
-
-	bool isWindowOpen_ = false;
-	bool isMinimized_ = false;
-	std::string menuTitle_ = "CustomChat";
-
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
-	
 	*/
 };
 
